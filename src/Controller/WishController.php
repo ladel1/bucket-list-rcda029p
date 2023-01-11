339012901d830f526e1e24394cbe6785fcbfd4c3
Adel LATIBI <alatibi@campus-eni.fr>
2023-01-11 16:05:37 +0100

    Correction TP 5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,20 +13,43 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class WishController extends AbstractController
 {
+
+    private $wishRepository;
+
+    public function __construct(WishRepository $wishRepository)
+    {
+        $this->wishRepository = $wishRepository;
+    }
+
+    /**
+     * @Route("/ajouter", name="ajouter")
+     */
+    public function add( ): Response
+    {
+        $wish = new Wish();
+        $wish->setTitle("Test 3")
+             ->setDescription("blablalblablalb")
+             ->setAuthor("Pierre");
+        $this->wishRepository->add($wish,true);
+        return $this->render('wish/add.html.twig');
+    }
+
     /**
      * @Route("/{id}", name="detail", requirements={"id"="\d+"})
      */
-    public function detail($id): Response
+    public function detail(Wish $wish): Response
     {
-        return $this->render('wish/detail.html.twig');
+        return $this->render('wish/detail.html.twig',compact("wish"));
     }
 
     /**
      * @Route("/", name="list")
      */
-    public function list(): Response
+    public function list( ): Response
     {
-        return $this->render('wish/list.html.twig');
+        return $this->render('wish/list.html.twig',[
+            "wishes"=> $this->wishRepository->findBy(["isPublished"=>true],["dateCreated"=>"DESC"])
+        ]);
     }
     
     
